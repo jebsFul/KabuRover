4aa859ea25547619810689d6f9ccc83f25ab4586
jebsFul <t.faflun@gmail.com>
2026-06-06 16:06:14 +0200

    123

--- git_push.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set /p msg="Commit message: "
-git add .
-git commit -m "%msg%"
-git push
---
+++ git_push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+read -p "Commit message: " msg
+git add .
+git commit -m "$msg"
+git push
